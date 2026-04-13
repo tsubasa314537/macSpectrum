a925237f988a8f9a794453c81272ed45ab77c2bb
guopeng <peng.guo@china.nttdata.com>
2026-04-13 16:38:02 +0800

    Initial Commit

---
+++ MacSpectrum/MacSpectrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSpectrumApp.swift
+//  MacSpectrum
+//
+//  Created by 郭鹏 on 2026/4/13.
+//
+
+import SwiftUI
+
+@main
+struct MacSpectrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacSpectrumTests/MacSpectrumTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacSpectrumTests.swift
+//  MacSpectrumTests
+//
+//  Created by 郭鹏 on 2026/4/13.
+//
+
+import Testing
+@testable import MacSpectrum
+
+struct MacSpectrumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
